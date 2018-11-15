5d99fc19b88228fd144bd79b56abdcd3925f096a
Simon <simongram@me.com>
2018-11-15 13:09:38 +0100

    Update getMove.fsx

--- getMove.fsx
+++ getMove.fsx
@@ -2,5 +2,17 @@ type pit = int
 type board = pit list
 type player = Player1 | Player2
 
+let printBoard (b: board) : unit =
+  printfn "  %A %A %A %A %A %A" b.[12] b.[11] b.[10] b.[9] b.[8] b.[7]
+  printfn "%A             %A" b.[0] b.[13]
+  printfn "  %A %A %A %A %A %A" b.[1] b.[2] b.[3] b.[4] b.[5] b.[6]
+
 let getMove (b: board) (p: player) (q: string) : pit =
-  
+  printBoard b
+  printfn "%A %A" p q
+  let userInput : pit = int (System.Console.ReadLine())
+  userInput
+
+let testBoard : board = [0;3;3;3;3;3;3;3;3;3;3;3;3;0]
+
+printfn "%A" (getMove testBoard Player1 "Please choose a pit")
