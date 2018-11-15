4b585088e9fcc4349a5aa2eb9fb5412690951382
Simon <simongram@me.com>
2018-11-15 14:22:19 +0100

    Update distribute.fsx

--- distribute.fsx
+++ distribute.fsx
@@ -2,4 +2,11 @@ type pit = int
 type board = pit list
 type player = Player1 | Player2
 
-let distribute (b:board) (p:player) (i:pit) : board * player * pit
+let distribute (b:board) (p:player) (i:pit) : board * player * pit =
+  let mutable beanCount = b.[i]
+  let mutable pitIndex = i + 1
+  while beanCount > 0 do
+    b.[pitIndex] <- b.[pitIndex] + 1
+    beanCount <- beanCount - 1
+    pitIndex <- pitIndex + 1
+  
