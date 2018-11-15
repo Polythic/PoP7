035c74e66f3cd45802e51997f949e918ca953ee6
Simon <simongram@me.com>
2018-11-15 12:41:53 +0100

    Update isGameOver.fsx

--- isGameOver.fsx
+++ isGameOver.fsx
@@ -1,8 +1,21 @@
 type pit = int
 type board = pit list
-let isGameOver : b:board -> bool =
-let mutable i = 1
-while b.i = [] and i < 7 do
-  i <- i + 1
-if b.i > 0
-    return false
+let isGameOver (b: board) : bool =
+  let mutable i = 1
+  while b.[i] = 0 && i < 7 do
+    i <- i + 1
+    printfn "%A" i
+  if i = 7 then
+    true
+  else
+    i <- 7
+    while b.[i] = 0 && i < 13 do
+      i <- i + 1
+      printfn "%A" i
+    if i = 13 then
+      true
+    else
+      false
+
+let testBoard : board = [0;0;0;3;0;0;0;0;0;0;0;0;0;0]
+printfn "%A" (isGameOver testBoard)
