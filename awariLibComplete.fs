276980638921c78cd6fd5e77bd49b9fc20a589b0
Simon <simongram@me.com>
2018-11-15 14:21:36 +0100

    Update awariLibComplete.fs

--- awariLibComplete.fs
+++ awariLibComplete.fs
@@ -5,8 +5,8 @@ type player = Player1 | Player2
 
 // intentionally many missing implementations and additions
 let printBoard (b: board) : unit =
-  printfn "  %A %A %A %A %A %A" b.[12] b.[11] b.[10] b.[9] b.[8] b.[7]
-  printfn "%A             %A" b.[0] b.[13]
+  printfn "  %A %A %A %A %A %A" b.[13] b.[12] b.[11] b.[10] b.[9] b.[8]
+  printfn "%A             %A" b.[0] b.[7]
   printfn "  %A %A %A %A %A %A" b.[1] b.[2] b.[3] b.[4] b.[5] b.[6]
 
 let getMove (b: board) (p: player) (q: string) : pit =
