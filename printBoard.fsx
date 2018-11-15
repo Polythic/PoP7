dc81d2668dfb89e1f7fd1a83fd00872e954b3b60
Simon <simongram@me.com>
2018-11-15 11:57:20 +0100

    Update printBoard.fsx

--- printBoard.fsx
+++ printBoard.fsx
@@ -1,7 +1,7 @@
 type pit = int
 type board = pit list
 let printBoard (b: board) : unit =
-  printfn "  %A %A %A %A %A %A" b.[7] b.[8] b.[9] b.[10] b.[11] b.[12]
+  printfn "  %A %A %A %A %A %A" b.[12] b.[11] b.[10] b.[9] b.[8] b.[7]
   printfn "%A             %A" b.[0] b.[13]
   printfn "  %A %A %A %A %A %A" b.[1] b.[2] b.[3] b.[4] b.[5] b.[6]
 
