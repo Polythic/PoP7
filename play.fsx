92b38c7fcdf4aa6312d4b280cbee913140205e12
AlexzanderE <43817814+AlexzanderE@users.noreply.github.com>
2018-11-19 11:34:27 +0100

    Update play.fsx

--- play.fsx
+++ play.fsx
@@ -7,3 +7,16 @@ if isGameOver = true && b.isHome(player1) > b.isHome(player2)
 elif isGameOver = true && b.isHome(player2) > b.isHome(player1)
   printf "player2 vinder"
 then board = [0;3;3;3;3;3;3;3;3;3;3;3;3;0]
+
+
+let rec play (b : board) (p : player) : board = 
+  if isGameOver b then
+    b
+  else
+    let newB = turn b p
+    let nextP =
+      if p = Player1 then
+        Player2
+      else
+        Player1 
+    play newB nextP
