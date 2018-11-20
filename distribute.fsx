6bb21a32165a79416c3dfd07282451f49fdb5b5e
Simon <simongram@me.com>
2018-11-20 10:48:57 +0100

    Nu virker lortet

--- distribute.fsx
+++ distribute.fsx
@@ -1,12 +1,13 @@
 type pit = int
-type board = pit list
+type board = pit array
 type player = Player1 | Player2
 
 let distribute (b:board) (p:player) (i:pit) : board * player * pit =
   let mutable beanCount = b.[i]
   let mutable pitIndex = i + 1
+  let mutable newBoard = b
   while beanCount > 0 do
-    b.[pitIndex] <- b.[pitIndex] + 1
+    newBoard.[pitIndex] <- (newBoard.[pitIndex] + 1)
     beanCount <- beanCount - 1
     pitIndex <- pitIndex + 1
-  
+  (newBoard,p,pitIndex)
