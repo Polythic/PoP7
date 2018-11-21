86a5cb74a1c3dc602a12988e0f608eb337abb2c2
Simon <simongram@me.com>
2018-11-21 18:59:35 +0100

    Update awariTestApp.fsx

--- awariTestApp.fsx
+++ awariTestApp.fsx
@@ -1,5 +1,5 @@
 type pit = int
-type board = int list
+type board = int array
 type player = Player1 | Player2
 let isHome (b:board) (p:player) (i:pit) : bool =
   if p = Player1 && i = 7 then
@@ -8,10 +8,9 @@ let isHome (b:board) (p:player) (i:pit) : bool =
     true
   else
     false
-    
+
     printfn "Whitebox-testning af funktion isHome"
     printfn " Branch: 1a - %b" (isHome [] = )
     printfn " Branch: 2a - %b" (isHome [] = )
     printfn " Branch: 3a - %b" (isHome [] = )
     Printfn " Branch: 4a - ‰b" (isHome [] = )
-    
