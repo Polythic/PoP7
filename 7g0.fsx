b73fec2e26e8f64b7b4a70766a0666f2e82c5db0
Simon <simongram@me.com>
2018-11-21 17:04:34 +0100

    Update 7g0.fsx

--- 7g0.fsx
+++ 7g0.fsx
@@ -1,4 +1,13 @@
 open Awari
 let startBoard: board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
-printBoard(play startBoard Player1)
+let finalBoard = (play startBoard Player1)
+let winner =
+  if finalBoard.[0] > finalBoard.[7] then
+    "Winner is Player2"
+  elif finalBoard.[0] < finalBoard.[7] then
+    "Winner is Player1"
+  else
+    "Tie - No winner!"
+printBoard finalBoard
 printfn "Game over!"
+printfn "%s" winner
