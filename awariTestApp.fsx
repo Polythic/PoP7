f22efe7fd394eb5f6e0352cc95578fb4f3e15e68
Simon <simongram@me.com>
2018-11-21 20:06:01 +0100

    Update awariTestApp.fsx

--- awariTestApp.fsx
+++ awariTestApp.fsx
@@ -1,23 +1,8 @@
 open Awari
-(*
-type pit = int
-type board = int array
-type player = Player1 | Player2
-let isHome (b:board) (p:player) (i:pit) : bool =
-  if p = Player1 && i = 7 then
-    true
-  elif p = Player2 && i = 0 then
-    true
-  else
-    false
-*)
 let testBoard : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
 
 printfn "Whitebox-testning af funktion isHome"
 printfn " Branch: 1a - %b" (isHome testBoard Player1 7 = true)
-//printfn " Branch: 2a - %b" (p = Player2 & i = 7)
-//printfn " Branch: 3a - %b" (isHome [] = )
-//Printfn " Branch: 4a - ‰b" (isHome [] = )
 
 printfn "Whitebox-testning af funktion distribute"
 printfn " Branch: 1a - %b" (distribute testBoard Player1 1 = ([|0;0;4;4;4;3;3;0;3;3;3;3;3;3|], Player1, 4))
