f34619fe24baf2207ccf0578282f28d62d621dba
Simon <simongram@me.com>
2018-11-21 20:35:21 +0100

    Update awariTestApp.fsx

--- awariTestApp.fsx
+++ awariTestApp.fsx
@@ -1,5 +1,5 @@
 open Awari
-let testBoard : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
+let mutable testBoard : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
 
 printfn "Whitebox-testning af funktion isHome"
 printfn " Branch: 1a - %b" (isHome testBoard Player1 7 = true)
@@ -10,7 +10,10 @@ printfn " Branch: 3c - %b" (isHome testBoard Player1 5 = false)
 printfn " Branch: 3d - %b" (isHome testBoard Player2 1 = false)
 
 printfn "Whitebox-testning af funktion distribute"
-printfn " Branch: 1a - %b" (distribute testBoard Player1 1 = ([|0;0;4;4;4;3;3;0;3;3;3;3;3;3|], Player1, 4))
-//printfn " Branch: 2a - %b" (p = Player2 & i = 7)
-//printfn " Branch: 3a - %b" (isHome [] = )
-//Printfn " Branch: 4a - ‰b" (isHome [] = )
+printfn " Branch: 1a - %b" (distribute testBoard Player1 4 = ([|0;3;3;3;0;4;4;1;3;3;3;3;3;3|], Player1, 7))
+testBoard <- [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|] // reset testBoard
+printfn " Branch: 2a - %b" (distribute testBoard Player2 11 = ([|1;3;3;3;3;3;3;0;3;3;3;0;4;4|], Player2, 0))
+testBoard <- [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|] // reset testBoard
+printfn " Branch: 3a - %b" (distribute testBoard Player1 11 = ([|0;4;3;3;3;3;3;0;3;3;3;0;4;4|], Player1, 1))
+testBoard <- [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|] // reset testBoard
+printfn " Branch: 4a - %b" (distribute testBoard Player2 4 = ([|0;3;3;3;0;4;4;0;4;3;3;3;3;3|], Player2, 8))
