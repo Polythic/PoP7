57873f6b0ae2e319db31f79f2156c5602f5b574d
AlexzanderE <43817814+AlexzanderE@users.noreply.github.com>
2018-11-21 20:36:17 +0100

    Update awariTestApp.fsx

--- awariTestApp.fsx
+++ awariTestApp.fsx
@@ -1,16 +1,27 @@
 open Awari
-let testBoard : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
+let testBoard1 : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
+let testBoard2 : board = [|0;3;3;3;3;3;3|]
+let testBoard3 : board = [|0;3;3;3;3;3;3|]
+let testBoard4 : board = [||]
 
-printfn "Whitebox-testning af funktion isHome"
-printfn " Branch: 1a - %b" (isHome testBoard Player1 7 = true)
-printfn " Branch: 2a - %b" (isHome testBoard Player2 0 = true)
-printfn " Branch: 3a - %b" (isHome testBoard Player2 7 = false)
-printfn " Branch: 3b - %b" (isHome testBoard Player1 0 = false)
-printfn " Branch: 3c - %b" (isHome testBoard Player1 5 = false)
-printfn " Branch: 3d - %b" (isHome testBoard Player2 1 = false)
 
-printfn "Whitebox-testning af funktion distribute"
-printfn " Branch: 1a - %b" (distribute testBoard Player1 1 = ([|0;0;4;4;4;3;3;0;3;3;3;3;3;3|], Player1, 4))
+printfn "Whitebox-testing af funktion isHome"
+printfn " Branch: 1a - %b" (isHome testBoard1 Player1 7 = true)
+printfn " Branch: 2a - %b" (isHome testBoard1 Player2 0 = true)
+printfn " Branch: 3a - %b" (isHome testBoard1 Player2 7 = false)
+printfn " Branch: 3b - %b" (isHome testBoard1 Player1 0 = false)
+printfn " Branch: 3c - %b" (isHome testBoard1 Player1 5 = false)
+printfn " Branch: 3d - %b" (isHome testBoard1 Player2 1 = false)
+
+printfn "Whitebox-testing af funktionen isGameOver"
+printfn "Branch: 1a - %b" (isGameOver testBoard1 = true)
+printfn "Branch: 2a - %b" (isGameOver testBoard2 = false)
+printfn "Branch: 3a - %b" (isGameOver testboard3 = false)
+printfn "Branch: 4a - %b" (isGameOver testboard4 = false)
+
+
+printfn "Whitebox-testing af funktion distribute"
+printfn " Branch: 1a - %b" (distribute testBoard1 Player1 1 = ([|0;0;4;4;4;3;3;0;3;3;3;3;3;3|], Player1, 4))
 //printfn " Branch: 2a - %b" (p = Player2 & i = 7)
 //printfn " Branch: 3a - %b" (isHome [] = )
 //Printfn " Branch: 4a - ‰b" (isHome [] = )
