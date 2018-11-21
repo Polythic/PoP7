67c04cb0203cf5b15bfa5c23025b71cb9a1257dd
Simon <simongram@me.com>
2018-11-21 20:03:01 +0100

    Update awariTestApp.fsx

--- awariTestApp.fsx
+++ awariTestApp.fsx
@@ -12,8 +12,8 @@ let testBoard : board = [|0;3;3;3;3;3;3;0;3;3;3;3;3;3|]
 
 printfn "Whitebox-testning af funktion isHome"
 printfn " Branch: 1a - %b" (isHome testBoard Player1 7 = true)
-printfn " Branch: 2a - %b" (isHome testboard Player1 8 = true)
-printfn " Branch: 3a - %b" (isHome testboard Player2 7 = true)
-Printfn " Branch: 1b - ‰b" (isHome testboard Player2 0 = true)
-printfn " Branch: 2b - %b" (isHome testboard Player1 0 = true)
-printfn " Branch: 3b - %b" (isHome testboard Player2 1 = true)
+printfn " Branch: 2a - %b" (isHome testBoard Player2 0 = true)
+printfn " Branch: 3a - %b" (isHome testBoard Player2 7 = false)
+Printfn " Branch: 3b - ‰b" (isHome testBoard Player1 0 = false)
+printfn " Branch: 3c - %b" (isHome testBoard Player1 5 = false)
+printfn " Branch: 3d - %b" (isHome testBoard Player2 1 = false)
